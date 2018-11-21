c03b080962339fab1c654dcacb0a806a48a48a05
zaidradd94 <zaidrddad@gmail.com>
2018-11-21 15:15:12 +0200

    port3

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var app = require('./server-config.js');
 
-var port = 4568;
-
-app.listen(process.env.PORT || port);
+//var port = 4568;
+var port = process.env.PORT || 8000;
+//app.listen(process.env.PORT || port);
 // app.listen(port);
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
 console.log('Server now listening on port ' + port);
